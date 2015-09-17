98ab63dd4b3313367984edb39a3a82d38f58257b
stevenjchang <stevenjchang@gmail.com>
2015-09-17 00:30:55 -0700

    next2

---
+++ project1.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Learn JavaScript</title>
+		<script>
+			// alert('Hello World');
+
+
+			var firstName = 'John';
+
+			alert(firstName);
+
+
+		</script>
+	</head>
+	<body>
+		<h1>Welcome</h1>
+	</body>
+</html>
