6c69dca61b433d16e026d1778324e24003e47452
stevenjchang <stevenjchang@gmail.com>
2016-03-22 23:06:17 +0800

    CB Day 1
    first commit of my mock-up of codingbuddytutorial.com

---
+++ codingbuddy/readme.js
@@ -0,0 +1,75 @@
+Okay, lets do this shit.
+
+CodingBuddy.com, or
+codingbuddytutorial.com
+//establish a learning plan
+//build your schedule and system of learning
+//find a buddy
+//find a remote coding buddy
+//find a community
+//become a developer
+
+Make a Plan,
+Find a Buddy,
+and then put in the time
+/* Trying to become a coder?
+Trying to learn by yourself?
+*/
+This is my first real site. 
+This is exactly in my wheelhouse. 
+Lets get this to stage 1 by March 31st.
+
+What needs to be done in total, and what needs to be done by March 31st
+
+1) How-To-Guide on becoming a Js Developer  // operative word is "Guide"
+	A) Sequence
+	B) Resources
+	C) Set pace and schedule
+	D) Motivation, Road-Blocks, and tips
+
+2) How-To-Guide on Finding a Coding-Buddy, Buddies, or Community  // the operative word is ...
+
+3) Coding Buddy Database
+	A) Enter yourself into Database
+	B) Search Database
+	C) Connect to others through Database
+// in order to do this stuff, you will need some basic backend framework knowledge
+
+4) Online Coding Buddy
+	A) Site-wide chat???
+	B) Collaboration Tools Tutorial, and favorite picks
+
+5) Career Counseling
+
+// THAT is IT
+
+BIG PROJECT 1: Build an EMPTY Site  //the site should have everything layed out, just not working internally
+	//so what does it need?
+	1. Navbar
+	2. Homepage
+	3. Sign-up page (for coding buddy)
+	4. 'How-To' Section
+
+First Goal: //what is your first goal?
+	1. Navbar  // can you do it by tonight 3/22?
+	2. Homepage 
+		a. decide page color 
+		b. decide background image / color
+		c. make a tab  //the page should be a tutorial page 
+		d. make a form for entering your information
+		e. Entry Titles: //no need to actually write the article. just write the entry title and a short 1 line blurb.
+		f. 
+
+
+The articles are essentially the advice I already sort of give to people:
+
+Target Goals vs Time Goals.
+myth: setting the goal of x-hours a day gets it done
+
+--------------------------------------------------------------------------
+				DAY 1 - Tuesday 3/22 
+--------------------------------------------------------------------------
+// 1. Make a minimally functional "navbar"
+
+--------------------------------------------------------------------------
+	
