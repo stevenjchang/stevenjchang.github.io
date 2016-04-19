dd95ec25b3721bcba51b2c0b12d87f14df9d9738
stevenjchang <stevenjchang@gmail.com>
2016-04-19 15:47:40 +0800

    Moving Day
    changing the organization of all the root directories

---
+++ taiwan_listoffacebookgroups/readme.js
@@ -0,0 +1,136 @@
+Okay. April 15th.
+Lets Go!
+
+/* Lets build a very very simple project. This project is like dead simple. We should probably examine the ways in which this fails. I think it fails if you give up. I also think it fails if you waste time worrying about it and not doing it. */
+
+Steve is a self-taught Web Developer with a background in Psychology, Education, and Career Advising. He is passionate and is currently based out of Taiwan. 
+
+-----------------------------------------------------------------------------------
+so what needs to be done? What are all the aspects of this page?
+lets do sections first.
+1. Make the page
+	1.5 Optimize for Mobile
+2. Collect Data
+3. Search Engine Optimize it (dont worry about this part until you finish 1 and 2)
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+okay break it down even further. #1 is make the page. How do you want to make the page?
+What needs to be on it. 
+
+.navbar with just the name of the site. 
+	- get cool font for the name
+
+.jumbotron with a cool photo
+
+.sidebar with all the categories
+-----------------------------------------------------------------------------------
+DAY 1 
+Day 1 WRAP UP
+
+#Make A List of Objectives needed to make a MVP that can go live
+<ul>
+	completed list with links
+		- question is, how complete?
+		3 per category? (thats fine)
+		you should do this late a night, cuz its just busy work
+		--FUCK--But all of this, well, most of it is busy work. Stuff like installing a sidebar would be actual learning
+	each of the a hrefs to be linked and working
+	a website name
+
+<ul second level>
+	a category table of contents
+	a functional contact me page
+	make it mobile (you should learn alot from this)
+
+
+	a sign up mailing list 
+	a category bar on each side (with the inverse categories as options)
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+DAY and NIGHT 2
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+4/16
+/* Okay now is a good time to do some self-reflection
+Today is Saturday. Today is the day I left the house, went to the 24hr E Shuman internet cafe, and well you know the rest. I felt homeless wondering down the streets of Guting / Taipower / GongGuan station. I eventually settled on starbucks NTU. Stuggled but still coded. Eventually took a dinner search party walk, ended up feeling bloated and finally settled on taking a shit at mcdonalds. After taking a shit at mcdonalds, I decided to eat and code there. Was a pretty good decision. there's ALOT of ambient noise in this place, which helped me to not only focus but helped me to not fall asleep. There's also no internet here, which is quite interesting, because that also helped me focus. Wow no internet access really makes you somewhat focused. And the AirCon and lots of active people around really makes me happy. That's actually what I don't like about other coffee shops here. It's too quiet. And old-people ambient noise kind of bothers me (or maybe the lack to bg music to drown it out), but with younger people, I see some potentially for it to be a productive atmosphere. But then, that means you do not need to find a place where you can study at home. that's telling you that for you, that's not going to work. The starbucks is too quiet actually. You fall asleep. 
+
+Okay, but as far as coding CSS goes, I'm struggling, but I'm also learning. It's fun to have a concrete idea to struggle through. I just need to push through and get it done. But I think your idea is solid. Once you complete you page completely you can just minic it for all other regions. 
+*/
+-----------------------------------------------------------------------------------
+Okay do we know what your todo list now looks like?
+1. just get a minimal viable product out, what does that mean?
+FORGET about centering. Youve already figured out 30% of it. So just do that? (no wait, maybe that is wasting my time)
+2. get a basic side-bar going (this might be a good idea because you can just follow the video)
+3. improve the navbar so that the categories can be a drop-down when mobilized.
+-----------------------------------------------------------------------------------
+we still dont have much internet so lets do the categories
+
+Events and Activities
+Buy and Sell 
+Teaching / Jobs
+Language Exchange
+Foreigners in Taiwan
+
+Housing, Apartments, Roommates
+Food, Restaurants and Bars
+Outdoor / Hiking / Sports
+Entrepreners.
+Coding and Technology
+
+Non-Facebook Section 
+photo
+Board game
+comedy
+craft beer 
+visa help
+scooter
+volunteer & charity
+pets
+models actors
+News
+Foreigners For Taiwan Immigration
+
+taipei info exchange 
+foreign students fitness and Sports
+pass your old stuff
+hiking and riding in taipei
+taipei101 one-on-one language exchange
+taiwan actors group (TAG)
+model and talent
+musicians
+taipei trends: meet people, explore
+Taiwan News in English
+artist
+taiwan parties and events
+523 Mountaineering Association, Taiwan.
+
+-----------------------------------------------------------------------------------
+Okay cool that is the sections. 
+So seriously, what is your MVP, and what isnt your mvp.
+Your MVP needs to have a sidebar in it
+Thats it maybe. 
+
+update-OMG
+
+-OMG a sidebar is so freaken hard, actually all of this shit is pretty fucking hard. So i decided that the MVP just needs to drop-down nav bar. Plus you are incredibly tired. Today time is actually passing by super slow here at NTU starbucks. usually when you are awake and unable to focus time passes by hella fast. hella hella fast. today you are waiting for the time to tick down to midnight and fuck, each hours seems really really long. 
+-----------------------------------------------------------------------------------
+Its Monday, what are you goals this week?
+Well you need to get a MVP soon as possible I think. 
+Lets just do the dropdown. Lets just complete the dropdown real quick, like 30 minutes. 11:15am, for example. okay i was distracted, did it quick but watching basketball, so did it in an hour. 
+So what else, I think your site looks too patchy at this point. You need to make it look good again. which, primarily, means to get rid of stuff. 
+
+
+
+Im not focused this morning watching this nba game. and im also not certain where to go next. I mean, watching game and not focused aside... CSS is hard because you spend alot of time doing simple shit. like the solution (or what change is effected) seems so god damn simple. Each rabit hole is deeper, and has deeper ramifications. But i think when the actual site (as opposed to a branch-trial version gets too messy)
+
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+Day 4?
+What else needs to be done? 
+What were you doing last night?
+
+
+
+What should the Footer be?
