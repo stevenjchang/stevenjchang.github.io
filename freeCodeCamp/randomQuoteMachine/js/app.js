b9fe8c64ed90572d426c9b409c303e3d7aa60f1b
stevenjchang <stevenjchang@gmail.com>
2016-09-30 15:31:47 +0800

    tweet_box works
    works, but link does not include the quote

--- freeCodeCamp/randomQuoteMachine/js/app.js
+++ freeCodeCamp/randomQuoteMachine/js/app.js
@@ -2,14 +2,24 @@ var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', 
 var quotes = [["quote zero", "author zero"], ["quote one", "author one"], ["quote two", "author two"]];
 
 $(document).ready(function() {
+	var new_color = '';
+	var new_quote = '';
+
   $("#gen_new_quote").on("click", function(event){
-    var new_color = colors[Math.floor(Math.random() * colors.length)];
-    var new_quote = quotes[Math.floor(Math.random() * quotes.length)];
+    new_color = colors[Math.floor(Math.random() * colors.length)];
+    new_quote = quotes[Math.floor(Math.random() * quotes.length)];
     $('body').css("background-color", new_color);
     $('#gen_new_quote').css("background-color", new_color);
     $('#quote_text').html(new_quote[0]);
     $('#author_text').html(new_quote[1]);
   });
+
+  $("#tweet_box").on("click", function(){
+  	$('#twitter-share-button').attr("href", "https://twitter.com/intent/tweet?text=Hailo%20world");
+
+  });
+
+
 
 
 });
