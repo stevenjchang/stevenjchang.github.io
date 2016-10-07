ef70765c0648e7703e0fe8dc17ab029816105f9d
stevenjchang <stevenjchang@gmail.com>
2016-10-07 16:26:17 +0800

    randomQuoteMachine
    attempting to solve cross origin request issues

--- freeCodeCamp/randomQuoteMachine/js/app.js
+++ freeCodeCamp/randomQuoteMachine/js/app.js
@@ -6,20 +6,42 @@ $(document).ready(function() {
 	var new_quote = '';
 
   $("#gen_new_quote").on("click", function(event){
-    new_color = colors[Math.floor(Math.random() * colors.length)];
-    new_quote = quotes[Math.floor(Math.random() * quotes.length)];
-    $('body').css("background-color", new_color);
-    $('#gen_new_quote').css("background-color", new_color);
-    $('#quote_text').html(new_quote[0]);
-    $('#author_text').html(new_quote[1]);
+  
+    $.ajax({
+         url:"test_json_file.json",
+         crossDomain: true,
+         dataType: 'json', // Notice! JSONP <-- P (lowercase)
+         success:function(data){
+             // do stuff with json (in this case an array)
+             alert("Success");
+         },
+         error:function(){
+             alert("Error");
+         }      
+    });
+
+
+    // $.http( "test_json_file.json", function( data ) {
+    //   $( "#quote_box" ).html( data );
+    //   alert( "Load was performed." );
+    // });
+
+
+    // new_color = colors[Math.floor(Math.random() * colors.length)];
+    // new_quote = quotes[Math.floor(Math.random() * quotes.length)];
+    // $('body').css("background-color", new_color);
+    // $('#gen_new_quote').css("background-color", new_color);
+    // $('#quote_text').html(new_quote[0]);
+    // $('#author_text').html(new_quote[1]);
   });
 
   $("#tweet_box").on("click", function(){
   	$('#twitter-share-button').attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent(new_quote[0] + " " + new_quote[1]));
-
   });
 
-
-
-
 });
+
+
+
+
+
---
+++ practice_projects/rubykat/vampires.rb
@@ -0,0 +1,14 @@
+def ask_vampires
+	puts "What is your name?"
+	name = gets.chomp
+	puts "How old are you?"
+	name = gets.chomp
+	puts "What year were you born?"
+	year = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+	garlic_bread = gets.chomp
+	puts "Would you like to enroll in the company's health insurance? (y/n)"
+	health_insurance = gets.chomp
+end
+
+	ask_vampires
